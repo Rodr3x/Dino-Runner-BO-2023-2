6a1728b9d18563beb78ecbcb3de8124f17fe7ede
Sergio Rodrigo Paye Yujra <rodrigopaye524@gmail.com>
2023-03-30 22:40:51 -0400

    CLASS 1

---
+++ dino_runner/components/obstacles/Obstacle.py
@@ -0,0 +1,19 @@
+
+class Obstacle:
+    def __init__(self):
+        pass
+
+    def update(self): # para actualizar
+        pass
+
+    def draw(self): # para dibujar
+        pass    
+
+
+    # tarea
+    """
+    IMPLEMETAR LA CLASE CACTUS QUE HEREDE
+    DE OBSTACULO
+    CON CONSTRUCTOR
+    CLASE 1
+    """
